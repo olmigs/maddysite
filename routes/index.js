534d0736f6e26f11994a4ccc67450fc145954dc6
migs <migs@titter.biz>
2019-12-19 07:53:44 -0600

    added prod/dev toggle in index.js, not sure if working

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var sh = require('../public/javascripts/htmlsaver');
+var links = require('../public/src/links.json');
 var router = express.Router();
 
 router.use(express.static('./public'));
+var env = router.get('env');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
       res.render('index', {
           title: 'Madeleine Fellows | Welcome',
           sitename: 'madeleine fellows',
-          index_img: 'http://photos.madeleinefellows.com/galleries/home/sellwood.jpg'
+          index_img: 'http://photos.madeleinefellows.com/galleries/home/sellwood.jpg',
+          links: (env === 'development') ? links.prod : links.dev
       },
           function (err, html) {
               if (err) throw err;
